1c10832cc9e1e35ce209d178633f936bf394fda0
Youri Brosky <youri@brosky.net>
2024-10-23 17:03:58 +0200

    Added base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Add this line to specify the base path
   plugins: [react()],
 });
